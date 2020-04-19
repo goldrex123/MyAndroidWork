7b349e270642c6071e6ed8245de583d064d38509
kimjunyoung <goldrex123@naver.com>
2020-04-19 21:12:54 +0900

    계산기 완성

--- a003_listener/src/main/java/com/lec/android/a003_widget/Main2Activity.java
+++ a003_listener/src/main/java/com/lec/android/a003_widget/Main2Activity.java
@@ -15,6 +15,7 @@ public class Main2Activity extends AppCompatActivity {
     int clickNum = 0;
     int num1;
     int num2 = 1;
+    int pmmd = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,19 +28,79 @@ public class Main2Activity extends AppCompatActivity {
         Button btnMultiply = findViewById(R.id.btnMultiply);
         Button btnDivide = findViewById(R.id.btnDivide);
         Button btnResult = findViewById(R.id.btnResult);
+        Button btnClear = findViewById(R.id.btnClear);
 
         btnPlus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 num1 = Integer.parseInt(etResult.getText().toString());
                 etResult.setText("+");
+                clickNum = 0;
+                pmmd = 1;
+            }
+        });
+
+        btnMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                num1 = Integer.parseInt(etResult.getText().toString());
+                etResult.setText("-");
+                clickNum = 0;
+                pmmd = 2;
+            }
+        });
+
+        btnMultiply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                num1 = Integer.parseInt(etResult.getText().toString());
+                etResult.setText("*");
+                clickNum = 0;
+                pmmd = 3;
+            }
+        });
+
+        btnDivide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                num1 = Integer.parseInt(etResult.getText().toString());
+                etResult.setText("%");
+                clickNum = 0;
+                pmmd = 4;
             }
         });
 
         btnResult.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                etResult.setText(String.format("%d", (num1+num2)));
+                num2 = Integer.parseInt(etResult.getText().toString());
+
+
+                switch (pmmd) {
+                    case 1:
+                        etResult.setText("" + (num1 + num2));
+                        break;
+                    case 2:
+                        etResult.setText("" + (num1 - num2));
+                        break;
+                    case 3:
+                        etResult.setText("" + (num1 * num2));
+                        break;
+                    case 4:
+                        etResult.setText("" + (num1 / num2));
+                        break;
+
+                }
+            }
+        });
+
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                etResult.setText("0");
+                clickNum = 0;
+                num1 = 0;
+                num2 = 0;
             }
         });
     }
@@ -47,7 +108,7 @@ public class Main2Activity extends AppCompatActivity {
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btn0:
-                if(clickNum == 0) etResult.setText(" ");
+                if(clickNum == 0) etResult.setText("");
                 etResult.setText(etResult.getText().toString() + "0");
                 clickNum++;
                 break;
